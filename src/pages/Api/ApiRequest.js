44b96df24988628a4c2a1074800b546ef40c0d19
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-05-20 14:21:27 +0500

    updated base url

--- src/pages/Api/ApiRequest.js
+++ src/pages/Api/ApiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://company-portal-app.herokuapp.com/",
 })
 
 export async function postData(endpoint, data) {
